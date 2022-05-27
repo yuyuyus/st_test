f085fa308feb884cb00e355db56e981fcaacd49d
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-27 17:29:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,24 @@ import pandas as pd
             
 def problem():
     input1 = st.text_input('정답 단어')
-    input2 = st.text_input('헷갈리게하는 단어')
+    input2 = st.text_input('헷갈리게 하는 단어')
+    date=datetime.now().strftime('%Y-%m-%d')
+    df=pd.DataFrame({
+'날짜': date,
+'정답': input1,
+'헷갈': input2
+})
+return df
+
+'''
     if st.button('만들기'):
-        option = st.radio("문제", (input1, input2))
+        option = st.radio("문제", ('미선택',input1, input2))
         if option == input1:
             st.success('정답입니다.')
+''' 
 
 problem()
-
+if st.button('만들기'):
+    st.table(df)
 
 
