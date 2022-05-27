c37dede5babfdd428c0e126e560abb0c1345c664
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-05-27 17:10:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,9 @@ import pandas as pd
 
 st.title('내가 찾은 단어 담벼락')
 
-#@st.cache(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 
-input = st.text_input('추가할 단어를 써 주세요.')
-a = st.text_input('정답 단어')
-b = st.text_input('헷갈리게하는 단어')
-if st.button('만들기'):
-    option = st.radio("문제", (a, b))
-    if option == a:
-        st.success('정답입니다.')
+
 
             
 def problem():
