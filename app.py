a2672b9f0544df1165dce4c47c88f3ee470d29e8
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-06-06 01:51:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,16 @@ import streamlit as st
 import datetime
 import pandas as pd
 
-@st.cache(allow_output_mutation=True)
-
-title = st.text_input('Movie title', 'Life of Brian')
-st.write('The current movie title is', title)
+#@st.cache(allow_output_mutation=True)
 
 
-'''
 w1 = st.text_input('정답 단어')
 w2 = st.text_input('헷갈리게 하는 단어')
 date=datetime.now().strftime('%Y-%m-%d')
 df=pd.DataFrame([date, w1, w2], columns=['날짜','정답','오답'])
 #return df
 st.table(df)
-
+'''
 print('당신은 번호 '+str(number)+'번 '+name+'이며, 혈액형은 '+blood+'형인 '+gender+'입니다. 맞습니까? 맞으면 다음 단계로 넘어가세요.')
 input_info=[number,name,blood,gender]
 globals()['input_info_{}'.format(number)]= input_info
