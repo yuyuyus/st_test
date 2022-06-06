c8a025f97ef4f8a485d9029293bba2d3a7839490
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-06-06 15:39:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_data(keyword1, keyword2):
         df.columns = ["날짜 및 기간(주)"] + list(range(1,len(keyword)+1)) 
         df.set_index("날짜 및 기간(주)", inplace=True)
         
-        fig, ax = plt.subplots()
+        #fig, ax = plt.subplots()
         ax = df.plot()
 
         ax.grid(alpha=0.3)
