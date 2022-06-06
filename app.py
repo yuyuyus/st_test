7fc989637243a56d4f0c6d06f11632005a0a75a6
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-06-06 13:29:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,17 +32,12 @@ def get_data2(keyword2):
     return df2
 
 # sidebar
-st.sidebar.write('''
-
-# :chart_with_upwards_trend: 구글 검색량 확인하기
-''')
+st.sidebar.write(''' # :chart_with_upwards_trend: 구글 검색량 확인하기''')
 
 st.sidebar.markdown(
     '''
     :smile:
-    :flag-kr:
-    :eyes:
-   지난 5년 동안 사람들이 구글과 유튜브에서 검색어를 검색한 빈도를 그래프로 확인해 봅니다. 
+   지난 5년 동안 사람들이 구글과 유튜브에서 검색어를 검색한 빈도:eyes:를 그래프로 확인해 봅니다. 
    시간 흐름에 따라 검색어에 대한 관심도가 가장 높을 때를 :100:으로 잡고 변화 양상을 보여줍니다. 
     ''')
 keyword = st.sidebar.text_input("검색어1를 입력하세요.(필수)", help="그래프가 파란색으로 그려집니다.")
@@ -59,13 +54,8 @@ if keyword:
             #st.dataframe(df2)
    
  
-    st.write('''
-             
-             ### 매주 검색량 변화 그래프(파랑:검색어1, 주황:검색어2)')
-    st.caption('(파랑:검색어1, 주황:검색어2)''')
-    '''
-    :smile:
-    '''
+    st.write(' ### 매주 검색량 변화 그래프 ')
+    st.subheader(''' :large_blue_square: : 검색어1,  :large_orange_square: : 검색어2''')
 
     fig, ax = plt.subplots()
     ax = df['검색량'].plot()
