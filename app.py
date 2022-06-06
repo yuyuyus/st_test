6e3ae29a2ff82390e7881141fcac492ab6395f3c
yuyuyus <103293394+yuyuyus@users.noreply.github.com>
2022-06-06 16:40:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from datetime import datetime
+import datetime
 
 
 from pytrends.request import TrendReq
